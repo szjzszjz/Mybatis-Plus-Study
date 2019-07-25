c9cac4d850fba317eeaf1f0ee6a9d4f87ee15684
石中君子 <312685889@qq.com>
2019-07-25 18:43:11 +0800

    提交附属文件

---
+++ src/main/java/com/szjz/mybatisplusstudy/MybatisPlusStudyApplication.java
@@ -0,0 +1,15 @@
+package com.szjz.mybatisplusstudy;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("com.szjz.mybatisplusstudy.dao")
+@SpringBootApplication
+public class MybatisPlusStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/szjz/mybatisplusstudy/MybatisPlusStudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.szjz.mybatisplusstudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybatisPlusStudyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
